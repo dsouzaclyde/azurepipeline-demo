bcb551d3d77f72e78a6e7c1da5d10c3de675fd97
Clyde D'Souza <ClydeD@datacom.co.nz>
2018-10-04 14:45:21 +1300

    initial commit

---
+++ demo-app/webapp/js/app.js
@@ -0,0 +1,5 @@
+﻿function getAddition(a, b) {
+	return a + b;
+}
+
+document.getElementById('test').innerHTML = "Test world " + getAddition(1,2);
---
+++ demo-app/webapp/js/test/app.test.js
@@ -0,0 +1,5 @@
+﻿describe("Check if the truth checker works", function () {
+	it("Handles a true string", function () {
+		expect(getAddition(1,3)).toBe(4);
+	});
+});
